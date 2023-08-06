d3ca1ca2490db21d966269dca54bfcd82a33ab0f
Paras <paras@pop-os.localdomain>
2023-08-06 12:24:34 +0530

    enable http connections
    Signed-off-by: Paras <paras@pop-os.localdomain>

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ class GetRequestReceiver(Resource):
 api.add_resource(GetRequestReceiver, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context='adhoc')
