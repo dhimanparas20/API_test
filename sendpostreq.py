03763481e0423c8b153560232ad65d33a37afada
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-01-08 11:29:43 +0530

    Create sendpostreq.py

---
+++ sendpostreq.py
@@ -0,0 +1,18 @@
+# Code to understand how to send post req in python
+import requests
+
+url = 'http://127.0.0.1:5000/changepass'
+payload = {
+    'email': 'gokew42094@tanlanav.com',
+    'currentpassword': '12345678',
+    'newpassword':"1234567890"
+
+}
+
+response = requests.post(url, data=payload)
+
+if response.status_code == 200:
+    print('Request successful!')
+    print('Response:', response.text)
+else:
+    print('Request failed with status code:', response.status_code)
